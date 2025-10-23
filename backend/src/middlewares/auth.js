dafe1e11a368987811627a428411d205387cc58f
hau0909 <hau.nguyen.personal@gmail.com>
2025-10-23 12:45:36 +0700

    feat(Auth): implement user registration and login functionality with JWT authentication

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+exports.verifyToken = (req, res, next) => {
+  const authorizationHeader = req.headers["authorization"];
+  const token = authorizationHeader.split(" ")[1];
+
+  if (!token) return res.status(403).json({ message: "No token provided" });
+
+  jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.status(401).json({ message: "Invalid token" });
+    req.userId = decoded.id;
+    next();
+  });
+};
